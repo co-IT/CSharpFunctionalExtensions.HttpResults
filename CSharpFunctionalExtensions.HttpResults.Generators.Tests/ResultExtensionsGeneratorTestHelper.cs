b00194853f62af30348b9a14784dc1f7a55feb52
Tim Schneider <43130816+DerStimmler@users.noreply.github.com>
2025-01-02 23:57:54 +0100

    test(generators): add tests for generator diagnostics

---
+++ CSharpFunctionalExtensions.HttpResults.Generators.Tests/ResultExtensionsGeneratorTestHelper.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace CSharpFunctionalExtensions.HttpResults.Generators.Tests;
+
+public static class ResultExtensionsGeneratorTestHelper
+{
+  public static IEnumerable<Diagnostic> RunGenerator(string sourceCode)
+  {
+    var syntaxTree = CSharpSyntaxTree.ParseText(sourceCode);
+
+    var references = new[]
+    {
+      MetadataReference.CreateFromFile(typeof(ResultExtensionsGenerator).Assembly.Location),
+      MetadataReference.CreateFromFile(typeof(IResultErrorMapper<,>).Assembly.Location),
+    }.ToList();
+
+    var compilation = CSharpCompilation.Create(
+      "TestAssembly",
+      [syntaxTree],
+      references,
+      new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary)
+    );
+
+    var generator = new ResultExtensionsGenerator();
+    var driver = CSharpGeneratorDriver.Create(generator);
+
+    driver.RunGeneratorsAndUpdateCompilation(compilation, out _, out var diagnostics);
+
+    return diagnostics;
+  }
+}
---
+++ CSharpFunctionalExtensions.HttpResults.Generators.Tests/Rules/DuplicateMapperRuleTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using Microsoft.CodeAnalysis;
+
+namespace CSharpFunctionalExtensions.HttpResults.Generators.Tests.Rules;
+
+public class DuplicateMapperRuleTests
+{
+  [Fact]
+  public void TestDuplicateMappers()
+  {
+    var sourceCode = """
+      using Microsoft.AspNetCore.Http;
+      using Microsoft.AspNetCore.Http.HttpResults;
+      using CSharpFunctionalExtensions.HttpResults;
+
+      public class DocumentCreationError
+      {
+        public required string DocumentId { get; init; }
+      }
+
+      public class DocumentCreationErrorMapper : IResultErrorMapper<DocumentCreationError, Conflict<string>>
+      {
+        public Func<DocumentCreationError, Conflict<string>> Map => error => TypedResults.Conflict(error.DocumentId);
+      }
+
+      public class DocumentCreationErrorMapper2 : IResultErrorMapper<DocumentCreationError, Conflict<string>>
+      {
+        public Func<DocumentCreationError, Conflict<string>> Map => error => TypedResults.Conflict(error.DocumentId);
+      }
+      """;
+
+    var diagnostics = ResultExtensionsGeneratorTestHelper.RunGenerator(sourceCode).ToList();
+
+    diagnostics.Count.Should().Be(1);
+
+    var diagnostic = diagnostics[0];
+
+    diagnostic.Id.Should().Be("CFEHTTPR002");
+    diagnostic.Severity.Should().Be(DiagnosticSeverity.Error);
+    diagnostic.GetMessage().Should().Be("Class 'DocumentCreationError' does have multiple IResultErrorMapper");
+  }
+}
