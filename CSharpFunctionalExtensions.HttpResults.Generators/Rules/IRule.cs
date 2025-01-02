126a1cf7de195d5838682a5babb8124077717e4a
Tim Schneider <43130816+DerStimmler@users.noreply.github.com>
2025-01-02 23:02:48 +0100

    style: cleanup & reformat

--- CSharpFunctionalExtensions.HttpResults.Generators/Rules/IRule.cs
+++ CSharpFunctionalExtensions.HttpResults.Generators/Rules/IRule.cs
@@ -4,17 +4,17 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 namespace CSharpFunctionalExtensions.HttpResults.Generators.Rules;
 
 /// <summary>
-/// Defines a rule for validating mapper classes.
+///   Defines a rule for validating mapper classes.
 /// </summary>
 internal interface IRule
 {
   /// <summary>
-  /// Gets the diagnostic descriptor for the rule.
+  ///   Gets the diagnostic descriptor for the rule.
   /// </summary>
   DiagnosticDescriptor RuleDescriptor { get; }
 
   /// <summary>
-  /// Checks the rule against a list of mapper classes.
+  ///   Checks the rule against a list of mapper classes.
   /// </summary>
   /// <param name="mapperClasses">The list of mapper classes to validate.</param>
   /// <returns>A collection of diagnostics representing rule violations.</returns>
